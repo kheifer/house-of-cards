a8e9cbda4ba1fa8f9202a95ddd4c3973612c8725
Kheifer Fuller and Shyamal Punekar <kheifer@>
2017-07-10 15:12:05 -0700

    add functionality to button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  
-})
+  var numbers = ["ace",2,3,4,5,6,7,8,9,10,"jack","queen","king"];
+  var suits = ["hearts","spades","clubs","diamonds"];
+
+  $(".btn").click(function() {
+    suits.forEach(function(suit) {
+      numbers.forEach(function(number) {
+        $("#cards").append('<li>' + number + ' of ' + suit + '</li>');
+      });
+      $("#deck").show();
+      $(".button").hide();
+    });
+  });
+  alert('helloworld');
+
+});
